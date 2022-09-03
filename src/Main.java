08a55b9ff014317a47d47074e257d9aa35f916f4
NurbolMarlisov <marlisaaliev195@gmai.com>
2022-09-03 16:19:54 +0600

    доработка

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("My one project");
+        String one ="My one project";
+        System.out.println(one);
     }
 }
